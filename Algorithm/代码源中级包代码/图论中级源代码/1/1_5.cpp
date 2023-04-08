938381e714f207b6f94c2e4620e971176269b73e
KicmaonIce <1532931259@qq.com>
2023-04-08 15:35:13 +0800

    2023/04/08,15:34=5

--- Algorithm/代码源中级包代码/图论中级源代码/1/1_5.cpp
+++ Algorithm/代码源中级包代码/图论中级源代码/1/1_5.cpp
@@ -10,43 +10,56 @@ stack<int> stk;
 vector<vector<int>> scc;
 vector<int> out, c;
 
-void dfs(int u) {
-	vis[u] = true;
-	for (auto v : e[u]) {
-		if (!vis[v]) dfs(v);
-	}
-	out.push_back(u);
+void dfs(int u)
+{
+    vis[u] = true;
+    for (auto v : e[u])
+    {
+        if (!vis[v])
+            dfs(v);
+    }
+    out.push_back(u);
 }
 
-void dfs2(int u) {
-	vis[u] = true;
-	for (auto v : erev[u]) {
-		if (!vis[v]) dfs2(v);
-	}
-	c.push_back(u);
+void dfs2(int u)
+{
+    vis[u] = true;
+    for (auto v : erev[u])
+    {
+        if (!vis[v])
+            dfs2(v);
+    }
+    c.push_back(u);
 }
 
-int main() {
-	scanf("%d%d", &n, &m);
-	for (int i = 0; i < m; i++) {
-		int u, v;
-		scanf("%d%d", &u, &v);
-		e[u].push_back(v);
-		erev[v].push_back(u);
-	}
-	for (int i = 1; i <= n; i++)
-		if (!vis[i]) dfs(i);
-	reverse(out.begin(), out.end());
-	memset(vis, 0, sizeof(vis));
-	for (auto u : out) if (!vis[u]) {
-		c.clear();
-		dfs2(u);
-		sort(c.begin(), c.end());
-		scc.push_back(c);
-	}
-	sort(scc.begin(), scc.end());
-	for (auto c : scc) {
-		for (auto u : c) printf("%d ", u);
-		puts("");
-	}
+int main()
+{
+    scanf("%d%d", &n, &m);
+    for (int i = 0; i < m; i++)
+    {
+        int u, v;
+        scanf("%d%d", &u, &v);
+        e[u].push_back(v);
+        erev[v].push_back(u);
+    }
+    for (int i = 1; i <= n; i++)
+        if (!vis[i])
+            dfs(i);
+    reverse(out.begin(), out.end());
+    memset(vis, 0, sizeof(vis));
+    for (auto u : out)
+        if (!vis[u])
+        {
+            c.clear();
+            dfs2(u);
+            sort(c.begin(), c.end());
+            scc.push_back(c);
+        }
+    sort(scc.begin(), scc.end());
+    for (auto c : scc)
+    {
+        for (auto u : c)
+            printf("%d ", u);
+        puts("");
+    }
 }
